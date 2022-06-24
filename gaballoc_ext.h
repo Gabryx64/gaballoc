9b028741e5c0f39f16382fb253719e16ee17996c
Gabryx64 <gabryx86_64@riseup.net>
2022-06-24 11:50:12 +0200

    gaballoc

--- gaballoc_ext.h
+++ gaballoc_ext.h
@@ -2,13 +2,28 @@
 #define LIBK_GABALLOC_EXT_H 1
 
 // Includes for defines
+#include"sys/memory/PMM.h"
+#include"str.h"
 
 // Defines
-#define GABALLOC_PAGE_SIZE ...
-#define GABALLOC_ALLOC     ...
-#define GABALLOC_FREE      ...
-#define GABALLOC_MEMCPY    ...
-#define GABALLOC_LOCK      ...
-#define GABALLOC_UNLOCK    ...
+#define GABALLOC_PAGE_SIZE PAGE_SIZE
+#define GABALLOC_ALLOC     PMM_alloc
+#define GABALLOC_FREE      PMM_free
+#define GABALLOC_MEMCPY    memcpy
+#define GABALLOC_LOCK      alloc_lock
+#define GABALLOC_UNLOCK    alloc_unlock
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// TODO: Make it do something when concurrency is implemented
+inline void alloc_lock()   {}
+inline void alloc_unlock() {}
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
