a3331348d02e0fb7d0231279ef4d1b59f8684917
Gabryx64 <gabryx86_64@riseup.net>
2022-08-08 16:19:19 +0200

    formatting

---
+++ gaballoc.d
@@ -0,0 +1,4 @@
+libk/gaballoc/gaballoc.o: libk/gaballoc/gaballoc.c \
+  libk/gaballoc/gaballoc.h libk/gaballoc_ext/gaballoc_ext.h \
+  kernel/sys/memory/PMM.h ../limine/limine.h kernel/sys/sys.h \
+  libk/str/str.h libk/mathutils/mathutils.h
--- gaballoc.h
+++ gaballoc.h
@@ -1,47 +1,48 @@
 #ifndef LIBK_GABALLOC_H
 #define LIBK_GABALLOC_H 1
-#include<stdbool.h>
-#include<stddef.h>
+#include <stdbool.h>
+#include <stddef.h>
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
-typedef struct BUFCTL_S
-{
-	struct SLAB_S* backptr;
-	struct BUFCTL_S* prev;
-	struct BUFCTL_S* next;
-	void* mem;
-	bool free;
+typedef struct BUFCTL_S {
+    struct SLAB_S* backptr;
+    struct BUFCTL_S* prev;
+    struct BUFCTL_S* next;
+    void* mem;
+    bool free;
 } __attribute__((packed)) bufctl_t;
 
-typedef struct SLAB_S
-{
-	struct SLAB_S* prev;
-	struct SLAB_S* next;
-	bufctl_t* firstbuf;
-	size_t refcount;
-	size_t maxrefs;
+typedef struct SLAB_S {
+    struct SLAB_S* prev;
+    struct SLAB_S* next;
+    bufctl_t* firstbuf;
+    size_t refcount;
+    size_t maxrefs;
 } __attribute__((packed)) slab_t;
 
-typedef struct CACHE_S
-{
-	bool large;
-	slab_t* slabs;
-	size_t size;
-	size_t align;
-	void* source;
-	void (*destructor)(void* ptr, size_t size);
+typedef struct CACHE_S {
+    bool large;
+    slab_t* slabs;
+    size_t size;
+    size_t align;
+    void* source;
+    void (*destructor)(void* ptr, size_t size);
 } cache_t;
 
-cache_t* cache_create(size_t size, size_t align, void (*constructor)(void* ptr, size_t size), void (*destructor)(void* ptr, size_t size));
+cache_t* cache_create(size_t size, size_t align,
+		      void (*constructor)(void* ptr, size_t size),
+		      void (*destructor)(void* ptr, size_t size));
 void cache_destroy(cache_t* cache);
 void* cache_alloc(cache_t* cache);
 void cache_free(cache_t* cache, void* ptr);
 
-inline void __alloc_noconstr__(void* ptr, size_t size){ (void)ptr; (void)size; }
+inline void __alloc_noconstr__(void* ptr, size_t size) {
+    (void)ptr;
+    (void)size;
+}
 #define NOCONSTRUCT __alloc_noconstr__
 
 #ifdef __cplusplus
